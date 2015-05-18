9732de4c3bce2cb296647ab936fb7d46ca573879
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-05-18 17:00:12 +0300

    Refs #24423 - Minor fix

--- redmine/helpdesk.sh
+++ redmine/helpdesk.sh
@@ -1,7 +1,8 @@
-!# /bin/bash
+#!/bin/bash
 
+mkdir -p /var/log/helpdesk
 cd /var/local/redmine
 bundle exec rake -f /var/local/redmine/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 host=$REDMINE_EMAIL_HOST username=$REDMINE_EMAIL_USERNAME password=$REDMINE_EMAIL_PASSWORD port=143 folder=Inbox \
 project=it-helpdesk move_on_success=read move_on_failure=failed \
-allow_override=project,tracker,priority,assigned_to,status,category,fixed_version >> /var/log/httpd/cronapache.log 2>&1
+allow_override=project,tracker,priority,assigned_to,status,category,fixed_version >> /var/log/helpdesk/helpdesk.log 2>&1
