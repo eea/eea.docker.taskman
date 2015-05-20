d6fa11da0b17358d960d94032f9206c773a0e49e
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-05-20 13:37:28 +0200

    Refs #24423 - Minor fixes for working cron jobs

--- redmine/helpdesk.sh
+++ redmine/helpdesk.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mkdir -p /var/log/helpdesk
-cd /var/local/redmine
-/usr/local/bin/bundle exec rake -f /var/local/redmine/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
-host=$REDMINE_EMAIL_HOST username=$REDMINE_EMAIL_USERNAME password=$REDMINE_EMAIL_PASSWORD port=143 folder=Inbox \
+LOG_DIR=/var/log/helpdesk
+REDMINE_PATH=/var/local/redmine
+
+#TODO replace with production values
+EMAIL_HOST=host
+EMAIL_USERNAME=user
+EMAIL_PASSWORD=pass
+EMAIL_FOLDER=Inbox
+
+/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
+host=$EMAIL_HOST username=$EMAIL_USERNAME password=$EMAIL_PASSWORD port=143 folder=$EMAIL_FOLDER \
 project=it-helpdesk move_on_success=read move_on_failure=failed \
-allow_override=project,tracker,priority,assigned_to,status,category,fixed_version >> /var/log/helpdesk/helpdesk.log 2>&1
+allow_override=project,tracker,priority,assigned_to,status,category,fixed_version >> $LOG_DIR/helpdesk.log 2>&1
