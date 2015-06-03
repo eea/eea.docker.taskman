176e4a98ecab18c48b8876236e6949b4e21cf098
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-06-03 15:26:21 +0300

    Refs #24423 - Minor refactoring

--- redmine/helpdesk.sh
+++ redmine/helpdesk.sh
@@ -6,11 +6,12 @@ REDMINE_PATH=/var/local/redmine
 #TODO replace with production values
 EMAIL_HOST=host
 PORT=143
+SSL=false
 EMAIL_USERNAME=user
 EMAIL_PASSWORD=pass
 EMAIL_FOLDER=Inbox
 
 /usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
-host=$EMAIL_HOST username=$EMAIL_USERNAME password=$EMAIL_PASSWORD port=$PORT folder=$EMAIL_FOLDER \
-project=it-helpdesk move_on_success=read move_on_failure=failed \
-allow_override=project,tracker,priority,assigned_to,status,category,fixed_version >> $LOG_DIR/helpdesk.log 2>&1
+host=$EMAIL_HOST username=$EMAIL_USERNAME password=$EMAIL_PASSWORD ssl=$SSL port=$PORT folder=$EMAIL_FOLDER \
+project=it-helpdesk move_on_success=processed move_on_failure=failed tracker=task no_permission_check=1 \
+unknown_user=accept >> $LOG_DIR/helpdesk.log 2>&1
--- redmine/taskman_email.sh
+++ redmine/taskman_email.sh
@@ -10,7 +10,7 @@ EMAIL_USERNAME=user
 EMAIL_PASSWORD=pass
 EMAIL_FOLDER=Inbox
 
-/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile --silent redmine:email:receive_imap RAILS_ENV="production" \
+/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 host=$EMAIL_HOST username=$EMAIL_USERNAME password=$EMAIL_PASSWORD port=$PORT folder=$EMAIL_FOLDER \
-project=it-helpdesk tracker=task move_on_success=processed move_on_failure=failed no_permission_check=1 \
-unknown_user=accept >> $LOG_DIR/helpdesk.log 2>&1
+project=it-helpdesk move_on_success=read move_on_failure=failed \
+allow_override=project,tracker,priority,status,category,fixed_version >> $LOG_DIR/helpdesk.log 2>&1
